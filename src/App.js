505dda410d253338f72227ab43b1cee12170d833
vita499411568 <30390165+vita499411568@users.noreply.github.com>
2022-09-05 02:06:31 +0800

    Typesetting

--- src/App.js
+++ src/App.js
@@ -25,31 +25,30 @@ const Error = () => {
 function App() {
   return (
     <div className="container">
-    <HashRouter>
-      <div className="nav-link">
-        <NavLink to="/">
-          <p>回到首頁</p>
-        </NavLink>
-        <NavLink to="/register">
-          <p>註冊頁面</p>
-        </NavLink>
-        <NavLink to="/login">
-          <p>登入頁面</p>
-        </NavLink>
-        <NavLink to="/todo">
-          <p>Todo 頁面</p>
-        </NavLink>
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Todo" element={<Todo />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-
-    </HashRouter>
-  </div>
+      <HashRouter>
+        <div className="nav-link">
+          <NavLink to="/">
+            <p>回到首頁</p>
+          </NavLink>
+          <NavLink to="/register">
+            <p>註冊頁面</p>
+          </NavLink>
+          <NavLink to="/login">
+            <p>登入頁面</p>
+          </NavLink>
+          <NavLink to="/todo">
+            <p>Todo 頁面</p>
+          </NavLink>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Todo" element={<Todo />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </HashRouter>
+    </div>
   );
 }
 
