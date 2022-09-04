b238f2e3d1138f61b9b4ac6820a8d2f9ece10615
vita499411568 <30390165+vita499411568@users.noreply.github.com>
2022-09-04 23:15:39 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
 import {
   HashRouter,
   NavLink,
+  Route,
+  Routes
 } from 'react-router-dom';
+import './App.css';
 
+const Home = () => {
+  return <p>這是 Home 頁面 </p>;
+}
 const Todo = () => {
-  return <p>這是 Todo 頁面 
-  </p>;
+  return <p>這是 Todo 頁面 </p>;
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
@@ -14,29 +18,38 @@ const Login = () => {
 const Register = () => {
   return <p>這是註冊頁面</p>;
 };
+const Error = () => {
+  return <p>Page is not found.</p>;
+}
 
 function App() {
   return (
     <div className="container">
-      <HashRouter>
-        <div className="nav-link">
-          <NavLink to="/">
-            <p>回到首頁</p>
-          </NavLink>
-          <NavLink to="/register">
-            <p>註冊頁面</p>
-          </NavLink>
-          <NavLink to="/login">
-            <p>登入頁面</p>
-          </NavLink>
-          <NavLink to="/todo">
-            <p>Todo 頁面</p>
-          </NavLink>
-        </div>
-        {/* Routes, Route 練習區 */}
-        {/* 練習區 */}
-      </HashRouter>
-    </div>
+    <HashRouter>
+      <div className="nav-link">
+        <NavLink to="/">
+          <p>回到首頁</p>
+        </NavLink>
+        <NavLink to="/register">
+          <p>註冊頁面</p>
+        </NavLink>
+        <NavLink to="/login">
+          <p>登入頁面</p>
+        </NavLink>
+        <NavLink to="/todo">
+          <p>Todo 頁面</p>
+        </NavLink>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Todo" element={<Todo />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+
+    </HashRouter>
+  </div>
   );
 }
 
